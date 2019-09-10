735c2599be2749699045a3773f987fa821de064f
zooRg <electroyz@mail.ru>
2019-09-10 23:13:42 +0300

    Добавил модель и CRUD + миграция таблицы book

---
+++ backend/models/Book.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "book".
+ *
+ * @property int $id
+ */
+class Book extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'book';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+        ];
+    }
+}
